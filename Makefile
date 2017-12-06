916d818116b4d495e077dc4629c92e8812c91c7b
Paul Gierz <pgierz@awi.de>
2017-12-06 08:52:51 +0100

    changes Makefile to mistral; adds netcdf tests

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 # Compiler Settings
-FC=mpiifort
-FCFLAGS=-O3 -xHost -DTREAT_OVERLAY -Duse_netCDF -Duse_comm_MPI1 -ip -align -fno-alias
+FC=mpif90
+FCFLAGS=-O3 -xHost -DTREAT_OVERLAY -Duse_netCDF -Duse_comm_MPI1 -ip -align -fno-alias \
+		-L/sw/rhel6-x64/netcdf/netcdf_fortran-4.4.3-intel14/lib \
+		-lnetcdff \
+		-I/sw/rhel6-x64/netcdf/netcdf_fortran-4.4.3-intel14/include \
+    -Wl,-rpath,/sw/rhel6-x64/netcdf/netcdf_fortran-4.4.3-intel14/lib
+
 FCFLAGS_MPIOM=-O3 -fpe0 -i4 -fp-model source -fast-transcendentals -no-prec-sqrt -xHost -heap-arrays -convert big_endian -fpp
 
 # Path to test files
-testfile_raw=/work/ollie/pgierz/mpiesm-pism-sc/experiments/basic/couple/pismr_greenland_mass_and_heat_flux_to_mpiom.nc
+testfile_raw=/pf/a/a270077/freshwater_tests/hosing_mask_IRD_1Sv_std.nc
 testfile_post=FBFLOMPIOM.ext8
 make:
 	$(FC) $(FCFLAGS) $(FCFLAGS_MPIOM) -o read_glacflux read_glacflux.f90
@@ -17,9 +22,7 @@ test:
 
 generate_test:
 	cdo -f ext -b 64B \
-		-add \
-		-selvar,bmr $(testfile_raw) \
-		-selvar,fmr $(testfile_raw) \
+		-selvar,area $(testfile_raw) \
 		$(testfile_post)
 
 	cdo -outputf,%12.8e,1 \
--- read_glacflux.f90
+++ read_glacflux.f90
@@ -1,11 +1,17 @@
 program test
+  ! PG: We need to load the netcdf module:
+  use netcdf
   implicit none
   INTEGER*8 :: IUGLAC1, IUGLAC2, IUGLAC3, IUGLAC4
-  REAL*8, allocatable :: GLACFLUXG(:,:), GLACFLUX2(:,:)
+  REAL*8, allocatable :: GLACFLUXG(:,:), GLACFLUX2(:,:), GLACFLUX3(:,:)
   CHARACTER (len=16) :: glacfname
   CHARACTER (len=16) :: glacfname2
+  CHARACTER (len=16) :: glacfnamenc
+  CHARACTER (len=16) :: fw_hosing_varname
+  INTEGER :: ncid, varid, x, y  ! PG: Allocate stuff for the netcdf test
   INTEGER, parameter :: ifid_glacflux=552
   INTEGER, parameter :: ifid_glacflux2=553
+  INTEGER, parameter :: ifid_glacflux3=554
   ALLOCATE(GLACFLUXG(254, 220))
   write (*,*) "*** EXT TEST ***"
   glacfname="FBFLOMPIOM.ext8"
@@ -17,7 +23,13 @@ program test
   write (*,*) "Reading GLACFLUXG..."
   read(ifid_glacflux) GLACFLUXG
   write (*,*) "GLACFLUXG finished"
-  ! write (*,*) GLACFLUXG
+  write (*,*) SHAPE(GLACFLUXG)
+  write (*,*) GLACFLUXG
+  
+  write(*,*) "################################################################"
+  write(*,*) GLACFLUXG(66,126)
+  write(*,*) GLACFLUXG(126,66)
+  
   close(ifid_glacflux)
   write (*,*) " *** END! ***"
 
@@ -35,5 +47,30 @@ program test
   close(ifid_glacflux2)
   write (*,*) "*** END! ***"
 
+  ! PG: Test 3 as netcdf version
+  write (*,*) "*** NETCDF TEST ***"
+  ALLOCATE(GLACFLUX3(254, 220)) ! PG: This will be the array where glacflux goes
+  fw_hosing_varname="area"
+  glacfnamenc="FBFLOMPIOM.nc"   ! PG: The filename
+  ! Open the file. NF90_NOWRITE says "read only, dude"
+  call check(nf90_open(glacfnamenc, NF90_NOWRITE, ncid)) 
+  ! get the varid
+  call check(nf90_inq_varid(ncid, fw_hosing_varname, varid))
+  ! ...and read the variable
+  call check(nf90_get_var(ncid, varid, GLACFLUX3))
+  write(*,*) "Here is the netcdf read GLACFLUX3:"
+  write(*,*) GLACFLUX3(:,:)
+  ! Close the file again:
+  call check(nf90_close(ncid))
+  write(*,*) "*** END! ***"
 
+
+CONTAINS
+  subroutine check(status)
+      integer, intent ( in) :: status
+      if(status /= nf90_noerr) then 
+          print *, trim(nf90_strerror(status))
+          stop "Stopped"
+      end if
+  end subroutine check  
 end program test
